57b19273f27a5fd02107c64d2844d350da83cca2
hover <hover_android@126.com>
2017-12-02 17:31:54 +0800

    1.提交

--- app/src/main/java/com/hw/github/MainActivity.java
+++ app/src/main/java/com/hw/github/MainActivity.java
@@ -3,6 +3,7 @@ package com.hw.github;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e("github","修改代码");
+        Toast.makeText(this,"master",Toast.LENGTH_SHORT).show();
     }
 }
